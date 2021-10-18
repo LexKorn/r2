50039d326f2e34f40787166c76901061ab08791e
Alex <fischer84@mail.ru>
2021-10-18 19:44:40 +0300

    FOOD

--- FOOD/js/js_73.js
+++ FOOD/js/js_73.js
@@ -1,3 +1,7 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+require ('es7-object-polyfill');
+
 import tabs from './modules/tabs';
 import calc from './modules/calc';
 import cards from './modules/cards';
